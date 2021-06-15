784a5c62d965cc278b2db7fb10939c24d5d3ea6a
Anton Chaparin <chaparin.anton@gmail.com>
2021-06-15 11:33:47 +0300

    remove some files

--- at/src/ex00/ImagesToChar/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-mkdir target
-javac -d target ./src/java/edu/school21/printer/app/Program.java ./src/java/edu/school21/printer/logic/ASCII_convert.java
-java -cp target edu.school21.printer.app.Program ./resources/it.bmp 0 .
--- at/src/ex01/ImagesToChar/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir target
-javac -d target ./src/java/edu/school21/printer/app/Program.java ./src/java/edu/school21/printer/logic/ASCII_convert.java
-jar cvmf ./src/manifest.txt ./target/images-to-chars-printer.jar -C target ./edu/school21/printer
-java -jar ./target/images-to-chars-printer.jar ./src/resources/it.bmp 0 .
