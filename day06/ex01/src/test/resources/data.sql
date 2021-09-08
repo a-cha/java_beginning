955e5ed0eb52959792799dfa592508e9a8a24ae3
Anton Chaparin <chaparin.anton@gmail.com>
2021-09-08 15:26:43 +0300

    Add test for jdbc connection

---
+++ day06/ex01/src/test/resources/data.sql
@@ -0,0 +1,10 @@
+insert into products (name, price) values ('prod1', 1);
+insert into products (name, price) values ('prod2', 2);
+insert into products (name, price) values ('prod3', 3);
+insert into products (name, price) values ('prod4', 4);
+insert into products (name, price) values ('prod5', 5);
+insert into products (name, price) values ('prod6', 6);
+insert into products (name, price) values ('prod7', 7);
+insert into products (name, price) values ('prod8', 8);
+insert into products (name, price) values ('prod9', 9);
+insert into products (name, price) values ('prod10', 10);
